5100c64d3c182a3eaf5fb7a9b9535c8410edae02
tfwittwer <55980052+tfwittwer@users.noreply.github.com>
2019-10-01 12:51:19 +0200

    Add files via upload

---
+++ ShiftLights/ShiftLights/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ShiftLights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rpmLbl = new System.Windows.Forms.Label();
+            this.gearLbl = new System.Windows.Forms.Label();
+            this.shiftLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rpmLbl
+            // 
+            this.rpmLbl.AutoSize = true;
+            this.rpmLbl.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rpmLbl.Location = new System.Drawing.Point(143, 54);
+            this.rpmLbl.Name = "rpmLbl";
+            this.rpmLbl.Size = new System.Drawing.Size(167, 50);
+            this.rpmLbl.TabIndex = 0;
+            this.rpmLbl.Text = "10000";
+            this.rpmLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // gearLbl
+            // 
+            this.gearLbl.AutoSize = true;
+            this.gearLbl.Font = new System.Drawing.Font("Courier New", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gearLbl.Location = new System.Drawing.Point(24, 54);
+            this.gearLbl.Name = "gearLbl";
+            this.gearLbl.Size = new System.Drawing.Size(51, 50);
+            this.gearLbl.TabIndex = 2;
+            this.gearLbl.Text = "5";
+            this.gearLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // shiftLbl
+            // 
+            this.shiftLbl.Location = new System.Drawing.Point(30, 9);
+            this.shiftLbl.Name = "shiftLbl";
+            this.shiftLbl.Size = new System.Drawing.Size(280, 35);
+            this.shiftLbl.TabIndex = 3;
+            this.shiftLbl.Text = "Shift";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 120);
+            this.Controls.Add(this.shiftLbl);
+            this.Controls.Add(this.gearLbl);
+            this.Controls.Add(this.rpmLbl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ShiftLights";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label rpmLbl;
+        private System.Windows.Forms.Label gearLbl;
+        private System.Windows.Forms.Label shiftLbl;
+    }
+}
+
---
+++ ShiftLights/ShiftLights/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iRacingSdkWrapper;
+using System.IO;
+
+namespace ShiftLights
+{
+    public partial class Form1 : Form
+    {
+        private readonly SdkWrapper wrapper;
+
+        // todo:
+        // shiftpoint afhankelijk maken van versnelling, configuratie laden en onthouden
+        // positie onthouden en herstellen
+
+        private int[] percentage;
+        private int[] redline;
+
+        public Form1()
+        {
+            InitializeComponent();
+            shiftLbl.Text = "";
+
+            if(ReadSettings())
+            {
+                // Create instance
+                wrapper = new SdkWrapper();
+                wrapper.TelemetryUpdateFrequency = 30;
+                // Listen to events
+                wrapper.TelemetryUpdated += OnTelemetryUpdated;
+                wrapper.SessionInfoUpdated += OnSessionInfoUpdated;
+                // Start it!
+                wrapper.Start();
+
+            }
+        }
+
+        private bool ReadSettings()
+        {
+            if(File.Exists("shiftlights.txt"))
+            {
+                percentage = new int[4];
+                redline = new int[6];
+                try
+                {
+                    using (StreamReader reader = new StreamReader("shiftlights.txt"))
+                    {
+                        string text = reader.ReadLine();
+                        string[] fields = text.Split(' ');
+                        for (int i = 0; i < 4; i++) percentage[i] = Convert.ToInt32(fields[i]);
+
+                        for (int i = 0; i < 6; i++) redline[i] = Convert.ToInt32(reader.ReadLine());
+
+                        return true;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error reading settings from shiftlights.txt!");
+                    return false;
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("shiftlights.txt not found!");
+                return false;
+            }
+        }
+
+        private void OnSessionInfoUpdated(object sender, SdkWrapper.SessionInfoUpdatedEventArgs e)
+        {
+            // Use session info...
+            // hier kan ergens de redline uitgelezen worden
+        }
+        private void OnTelemetryUpdated(object sender, SdkWrapper.TelemetryUpdatedEventArgs e)
+        {
+            // Use live telemetry...
+            float rpm = e.TelemetryInfo.RPM.Value;          
+
+            rpmLbl.Text = rpm.ToString("F0");
+
+            int gear = e.TelemetryInfo.Gear.Value;
+
+            int percent = Convert.ToInt32(rpm / (Redline(gear)/100));
+            if (percent >= percentage[3])
+            {
+                shiftLbl.BackColor = Color.Red;
+            }
+            else if (percent >= percentage[2])
+            {
+                shiftLbl.BackColor = Color.Orange;
+            }
+            else if (percent >= percentage[1])
+            {
+                shiftLbl.BackColor = Color.Yellow;
+            }
+            else if (percent >= percentage[0])
+            {
+                shiftLbl.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                shiftLbl.BackColor = Color.Blue;
+            }
+
+
+            if (gear == -1) gearLbl.Text = "R";
+            else if (gear == 0) gearLbl.Text = "N";
+            else gearLbl.Text = gear.ToString();
+            
+        }
+
+        private int Redline(int gear)
+        {
+            gear--; // shift to 0-based
+            if (gear < 0) gear = 0; // neutral or reverse
+            else if (gear > 5) gear = 5; // higher than 6th gear
+
+            return redline[gear];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Location = Properties.Settings.Default.Location;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Location = Location;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
+
